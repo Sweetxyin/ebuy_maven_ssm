d32314ba40c4c59581c78d1c09e4e629b7bac5cc
zxy_4.10 <1150137404@qq.com>
2019-04-10 18:03:38 +0800

    4.10用spring架构了修改密码2

--- src/main/java/com/lcvc/ebuy_maven_ssm/service/AdminService.java
+++ src/main/java/com/lcvc/ebuy_maven_ssm/service/AdminService.java
@@ -8,8 +8,14 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class AdminService {
+
+
+    @Resource
+    private AdminDao adminDao;
 
 
     /**
@@ -21,58 +27,23 @@ public class AdminService {
     public Admin login(String username, String password){
         //将密码加密后再进行比对
         password= SHA.getResult(password);
-        Admin admin=null;
-        //打开数据库，耗费资源巨大，建议项目中只打开一次
-        SqlSessionFactory sessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory();
-        //创建SqlSession，SqlSession类似于JDBC中的Connection，
-        SqlSession session = sessionFactory.openSession();
-        try {
-            //获取mapper接口代理对象
-            AdminDao adminDao = session.getMapper(AdminDao.class);
-            admin=adminDao.login(username, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Admin admin=adminDao.login(username, password);
         return admin;
     }
+
 
     /*
 	 * 张三丰
 	 * 20180330
 	 * 将新密码保存到数据库中
-	 * @return null表示密码更改成功，false表示密码更改失败
+	 * @return true表示密码更改成功，false表示密码更改失败
 	 */
     public boolean updatePassword(String newPass,Integer id){
         Boolean status=false;//默认编辑失败
         newPass= SHA.getResult(newPass);
-
-        //打开数据库，耗费资源巨大，建议项目中只打开一次
-        SqlSessionFactory sessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory();
-        //创建SqlSession，SqlSession类似于JDBC中的Connection，
-        SqlSession session = sessionFactory.openSession();
-        try {
-            //获取mapper接口代理对象
-            AdminDao adminDao = session.getMapper(AdminDao.class);
-            // 编写代码，判断是否编辑成功
-            if(adminDao.updatePassword(newPass,id)>0){
-                status=true;
-            }
-            session.commit();//提交事务，提交事务之后对数据库做的所有变更才能生效
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+        if(adminDao.updatePassword(newPass,id)>0){
+            status=true;
         }
         return status;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
