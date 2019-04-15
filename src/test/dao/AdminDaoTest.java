c40a176bb74731258101bac97e9820a3604bf04e
zxy_4.15 <1150137404@qq.com>
2019-04-15 11:53:49 +0800

    4.15用SpringJunitTest和Admindao实现改密码

--- src/test/dao/AdminDaoTest.java
+++ src/test/dao/AdminDaoTest.java
@@ -1,51 +1,47 @@
 package dao;
 
 import com.lcvc.ebuy_maven_ssm.dao.AdminDao;
-import com.lcvc.ebuy_maven_ssm.dao.SqlSessionFactoryUtil;
 import com.lcvc.ebuy_maven_ssm.model.Admin;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import com.lcvc.ebuy_maven_ssm.test.SpringJunitTest;
+import com.lcvc.ebuy_maven_ssm.util.SHA;
 import org.junit.Test;
 
-public class AdminDaoTest {
+import javax.annotation.Resource;
+
+public class AdminDaoTest extends SpringJunitTest {
+
+    @Resource
+    private AdminDao adminDao;
+
+    @Test
+    public void print(){
+        System.out.println("hello");
+    }
 
     @Test
     public void testLogin(){
-        Admin admin=null;
-        //打开数据库，耗费资源巨大，建议项目中只打开一次
-        SqlSessionFactory sessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory();
-        //创建SqlSession，SqlSession类似于JDBC中的Connection，
-        SqlSession session = sessionFactory.openSession();
-        try {
-            //获取mapper接口代理对象
-            AdminDao adminDao = session.getMapper(AdminDao.class);
-            admin=adminDao.login("user", "123456");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Admin admin=adminDao.login("user",  SHA.getResult("123456"));
         System.out.println(admin);
+        Admin admin2=adminDao.login("user",  SHA.getResult("12345611"));
+        System.out.println(admin2);
     }
     @Test
     public void testUpdatePassword() {
-        //打开数据库，耗费资源巨大，建议项目中只打开一次
+        /*//打开数据库，耗费资源巨大，建议项目中只打开一次
         SqlSessionFactory sessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory();
         //创建SqlSession，SqlSession类似于JDBC中的Connection，
         SqlSession session = sessionFactory.openSession();
         try {
             //获取mapper接口代理对象
-            AdminDao adminDao = session.getMapper(AdminDao.class);
+            AdminDao adminDao = session.getMapper(AdminDao.class);*/
             System.out.println(adminDao.updatePassword("111", 2));
-            session.commit();
+       /*     session.commit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (session != null) {
                 session.close();
             }
-        }
+        }*/
     }
 }
